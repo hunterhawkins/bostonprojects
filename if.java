4fa14533739b2037e5acf9ceb764c07cc29ff6fb
hunterhawkins <thehuntero@gmail.com>
2015-09-05 22:44:17 -0400

    If statement
    If statement practice

---
+++ if.java
@@ -0,0 +1,14 @@
+
+class apples {
+	public static void main (String args[]){
+		int test = 6;
+		
+		//== is a testing operator. 
+		if (test == 9) {
+			System.out.println("Yes");
+		}else{
+			System.out.println("This is else");
+		}
+		
+	}
+}
